792ba1dec984feac4f71e6d154db0e165a1cea9f
Dzmitry Yushkevich <ip.yshkevich@gmail.com>
2023-10-31 15:02:10 +0300

    find extra.css

--- 01_01e/script.js
+++ 01_01e/script.js
@@ -1,2 +1,14 @@
 const styleSheets = document.styleSheets;
 console.log(styleSheets);
+
+const styleSheetsArray = Array.from(styleSheets);
+const targetSheets = styleSheetsArray.find((styleSheets) => {
+  if(styleSheets?.href){
+    const patchName = new URL(styleSheets.href).pathname.split("/").pop();
+    if (patchName === "extra.css"){
+      return styleSheets;
+    }
+    
+  }
+})
+console.log(targetSheets)
